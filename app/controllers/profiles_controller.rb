bd663a0eb37b6a196c4c5c74f399f8ed2faf09b1
JordonGoodsir <jordon.goodsir1@gmail.com>
2020-07-25 09:46:12 +1000

    user validation on profile implemented

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,21 +1,23 @@
 class ProfilesController < ApplicationController
   before_action :set_profile, only: [:show, :edit, :update, :destroy]  
-  # before_action :authenticate_user!, except: :home
+  before_action :only_user, only: [:show, :edit] 
+  before_action :profile_check, only: [:home, :edit] 
 
   def home   
-
     begin
     if Profile.find_by(user_id: current_user.id) == nil 
       redirect_to new_profile_path
     end 
   rescue 
-  end
+  end  
+
   end
 
   # GET /profiles/1
   # GET /profiles/1.json
   def show  
-  @current_user = current_user.id
+  @current_user = current_user.id  
+
   end
 
   # GET /profiles/new
@@ -24,14 +26,16 @@ class ProfilesController < ApplicationController
   end
 
   # GET /profiles/1/edit
-  def edit
+  def edit   
+   
   end
 
   # POST /profiles
   # POST /profiles.json
   def create
     @profile = Profile.new(profile_params) 
-    @profile.user_id = current_user.id
+    @profile.user_id = current_user.id 
+    @profile.wallet = 0.00
 
     respond_to do |format|
       if @profile.save
@@ -72,7 +76,19 @@ class ProfilesController < ApplicationController
     # Use callbacks to share common setup or constraints between actions.
     def set_profile
       @profile = Profile.find(params[:id])
-    end
+    end 
+
+    def only_user  
+      if current_user.id != @profile.user_id 
+        redirect_to root_path
+      end
+    end 
+
+    def profile_check  
+      if current_user
+        @current_profile = Profile.find_by(user_id: current_user).id 
+      end
+    end 
 
     # Only allow a list of trusted parameters through.
     def profile_params
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_22_040530) do
+ActiveRecord::Schema.define(version: 2020_07_24_234435) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -62,7 +62,7 @@ ActiveRecord::Schema.define(version: 2020_07_22_040530) do
     t.integer "number"
     t.bigint "user_id", null: false
     t.string "picture"
-    t.integer "wallet"
+    t.float "wallet"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_profiles_on_user_id"
